69e324fb4ed6846132869597bc75d044b57df904
DOKERcom <dokercom228@gmail.com>
2021-03-08 23:05:08 +0200

    fustfix корректировка названия кнопки в майле.

--- php/registration.php
+++ php/registration.php
@@ -62,10 +62,10 @@ if(isset($_POST['login']) && isset($_POST['email']) && isset($_POST['password'])
 			<div><!--[if mso]>
 	  <v:roundrect xmlns:v='urn:schemas-microsoft-com:vml' xmlns:w='urn:schemas-microsoft-com:office:word' href='http://' style='height:36px;v-text-anchor:middle;width:200px;' arcsize='50%' strokecolor='#e6e6e8' fillcolor='#ff9b8f'>
 		<w:anchorlock/>
-		<center style='color:#2f353e;font-family:sans-serif;font-size:13px;font-weight:bold;'>Reset password</center>
+		<center style='color:#2f353e;font-family:sans-serif;font-size:13px;font-weight:bold;'>Confirm email</center>
 	  </v:roundrect>
 	<![endif]--><a href='https://gclipsa.com/pages/activation?token=".$tok."'
-	style='background-color:#ff9b8f;border:1px solid #e6e6e8;border-radius:18px;color:#2f353e;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:36px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;'>Reset password</a></div>
+	style='background-color:#ff9b8f;border:1px solid #e6e6e8;border-radius:18px;color:#2f353e;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:36px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;'>Confirm email</a></div>
 			<br><br>This link will be available within 24 hours.";
 		
 		$mail = new PHPMailer();
